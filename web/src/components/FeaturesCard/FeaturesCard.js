3c2ac00bc0b1b1d63510c665369d3c5eab385557
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-16 02:20:13 +0530

    Adding feature card component

---
+++ web/src/components/FeaturesCard/FeaturesCard.js
@@ -0,0 +1,51 @@
+// IMPORTING PACKAGES/MODULES
+import React from 'react'
+import './featuresCard.css'
+
+import { ArrowCircleRight } from '@mui/icons-material'
+import { Box, Card, Chip, IconButton, Typography } from '@mui/material'
+
+import { Link } from '@redwoodjs/router'
+
+const FeaturesCard = ({ title, content, image, tags, link }) => {
+  return (
+    <Card
+      className="features-card"
+      sx={(theme) => {
+        console.log(theme)
+        return {
+          borderColor:
+            theme.palette.mode === 'light'
+              ? 'hsl(214.3 31.8% 91.4)'
+              : 'rgba(230,230,230,0.4)',
+          backdropFilter: 'blur(10px)',
+        }
+      }}
+    >
+      <Box className="features-card-content">
+        <Typography variant="h5" className="feature-card-title">
+          {title}
+        </Typography>
+        <Typography variant="body1" className="feature-card-subtitle">
+          {content}
+        </Typography>
+        {tags && (
+          <Box className="feature-tags-container">
+            {tags.map((tag) => {
+              return <Chip key={tag} color="default" label={tag} />
+            })}
+          </Box>
+        )}
+        {link && (
+          <IconButton component={Link} to={link} className="feature-card-link">
+            {' '}
+            <ArrowCircleRight fontSize="large" />{' '}
+          </IconButton>
+        )}
+      </Box>
+      {image && <img src={image} className="feature-card-image" alt={title} />}
+    </Card>
+  )
+}
+
+export default FeaturesCard
