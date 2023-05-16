48aacd92ad3986bbec6b312382a6f1976aded9c5
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-16 15:30:23 +0530

    Updating landing page

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -1,82 +1,306 @@
-import { Box, Button, Typography } from '@mui/material'
+// IMPORTING PACKAGES/MODULES
+import { useEffect, useRef } from 'react'
 
-import { Link, routes } from '@redwoodjs/router'
+import { FreeBreakfast, Paid } from '@mui/icons-material'
+import { Box, Chip, Link, Typography } from '@mui/material'
+import createGlobe from 'cobe'
+
+import { Link as RedwoodLink, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import './landingPage.css'
+import Button from 'src/components/Button/Button'
+import FeaturesCard from 'src/components/FeaturesCard/FeaturesCard'
+import PriceCard from 'src/components/PriceCard/PriceCard'
+
 const LandingPage = () => {
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    let phi = 0
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+        { location: [37.7595, -122.4367], size: 0.03 },
+        { location: [40.7128, -74.006], size: 0.1 },
+      ],
+      onRender: (state) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi
+        phi += 0.01
+      },
+    })
+
+    return () => {
+      globe.destroy()
+    }
+  }, [])
+
   return (
     <>
-      <MetaTags title="Landing" description="Landing page" />
-      <Box>
+      <MetaTags
+        title="Home"
+        description="Track your application's performance effortlessly with Tracepath"
+      />
+      <>
         <Box component="header">
-          <Typography variant="h1">Tracepath</Typography>
-          <Box component="nav">
-            <Link href="#">Home</Link>
-            <Link href="#">Features</Link>
-            <Link href="#">Pricing</Link>
-            <Link href="#">Testimonials</Link>
-            <Link href="#">Contact</Link>
-          </Box>
-          <Button
-            variant="outlined"
-            size="medium"
-            onClick={() => console.log('Get Started')}
+          <Box
+            component="nav"
+            className="secondary-links"
+            sx={(theme) => {
+              return {
+                '&>a': {
+                  color:
+                    theme.palette.mode === 'light'
+                      ? 'common.black'
+                      : 'common.white',
+                },
+              }
+            }}
           >
-            Get Started
-          </Button>
+            <Link
+              component="a"
+              href="https://twitter.com/n00bgineer"
+              target="_blank"
+            >
+              Updates
+            </Link>
+          </Box>
+          <Box className="brand-info-container">
+            <img
+              src="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+              alt="Tracepath logo"
+              className="logo"
+            />
+            <Typography variant="body1" className="brand-name">
+              Tracepath
+            </Typography>
+          </Box>
+          <Box
+            component="nav"
+            className="primary-links"
+            sx={(theme) => {
+              return {
+                '&>a': {
+                  color:
+                    theme.palette.mode === 'light'
+                      ? 'common.black'
+                      : 'common.white',
+                },
+              }
+            }}
+          >
+            <Link component={RedwoodLink} href="#">
+              Signup
+            </Link>
+          </Box>
         </Box>
         <Box id="hero" component="section">
-          <img src="https://example.com/hero.png" alt="Tracepath Hero Image" />
-          <Typography variant="h2">
-            Visual Tracerouting and Application Monitoring
-          </Typography>
-          <Typography variant="body1">
-            Tracepath is a visual tracerouting and application monitoring system
-            that helps you understand the performance of your network and
-            applications.
-          </Typography>
-          <Button onClick={() => console.log('Learn More')}>Learn More</Button>
+          <Box className="hero-content-container">
+            <Typography variant="h1" className="hero-headline">
+              Application Monitoring Simplified
+            </Typography>
+            <Typography variant="body1" className="hero-subheadline">
+              Tracepath generates simplified performance reports for your web
+              apps from multiple locations across the world
+            </Typography>
+            <Box className="hero-cta-container">
+              <Button
+                variant="contained"
+                component={RedwoodLink}
+                size="large"
+                to={routes.signup()}
+                className="primary-grad"
+              >
+                Generate your app&rsquo;s report
+              </Button>
+            </Box>
+          </Box>
+          {/* <img src="https://example.com/hero.png" alt="Tracepath Hero " /> */}
         </Box>
         <Box id="features" component="section">
-          <Typography variant="h2">Features</Typography>
-          <ul>
-            <li>Visual tracerouting</li>
-            <li>Application monitoring</li>
-            <li>Affordable pricing</li>
-            <li>Historical data tracking</li>
-            <li>More features coming soon!</li>
-          </ul>
+          <Box className="section-title-container">
+            <Typography variant="h2" className="section-title">
+              Enhance your app&rsquo;s performance with Tracepath
+            </Typography>
+            <Typography variant="body1" className="section-subtitle">
+              Discover a user-friendly solution that empowers you to
+              effortlessly monitor and optimize your applications. Gain
+              actionable insights, track performance metrics, and enhance
+              application efficiency for a seamless user experience.
+            </Typography>
+          </Box>
+          <Box className="features-card-container">
+            <FeaturesCard
+              title="Simplified Metrics"
+              content="Tracepath simplifies metrics for effortless optimization and makes it accessible to even non-technical stakeholders. Track, analyze, and improve performance indicators with ease."
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684180433/tracepath/assets/Fractal_Cube_4_0001_uwjnom.png"
+              link={routes.signup()}
+            />
+            <FeaturesCard
+              title="Visual Tracerouting"
+              content="You can visualize the routing path of your network packets along with it's geographic location, allowing you to identify any potential bottlenecks or performance issues along the way."
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684179759/tracepath/assets/Atom_Bucky_1__R_tjxtpa.png"
+              link={routes.signup()}
+            />
+            <FeaturesCard
+              title="Multiple Servers"
+              content="Tracepath allows you to generate comprehensive reports across global locations, from Tokyo to New York. Gain valuable insights into application behavior from different geographical regions. "
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684181735/tracepath/assets/Torus_Stack_Taper_R_hnkmsm.png"
+              link={routes.signup()}
+            />
+            <FeaturesCard
+              title="Competitor Analysis"
+              content="Tracepath's competitor analysis feature, allows you to comparing your product's performance metrics against industry rivals. Gain valuable insights & surpass the competition."
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684180239/tracepath/assets/Block_Matrix_x3_0003_cvgebh.png"
+              tags={['🚧 In Progress']}
+            />
+            <FeaturesCard
+              title="Project-level tracking"
+              content="Tracepath tracks performance metrics across multiple applications. Gain comprehensive insights into the performance of each app and optimize their performance."
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684181079/tracepath/assets/Platonic_3_-_Icosa0003_g4lqsk.png"
+              tags={['🚧 In Progress']}
+            />
+            <FeaturesCard
+              title="TraceScore&trade;"
+              content="Tracepath evaluates and benchmarks your app's performance against multiple locations and uses multiple metrics like latency, response times, etc to create your app's TraceScore&trade;."
+              image="https://res.cloudinary.com/dgu9rv3om/image/upload/v1684181893/tracepath/assets/Cylinder_Short_Stack_x5_0003_y4w3f7.png"
+              tags={['🚧 In Progress']}
+            />
+          </Box>
         </Box>
         <Box id="pricing" component="section">
-          <Typography variant="h2">Pricing</Typography>
-          <ul>
-            <li>Free plan: Basic features</li>
-            <li>Pro plan: More features, higher limits</li>
-          </ul>
-          <Typography variant="body1">
-            The free plan is available now. The pro plan will be available soon.
+          <Box className="section-title-container">
+            <Typography variant="h2" className="section-title">
+              Pricing
+            </Typography>
+            <Typography variant="body1" className="section-subtitle">
+              A no-nonsense guide to Tracepath&rsquo;s pricing. Only free mode
+              is available for now.
+            </Typography>
+          </Box>
+          <Box className="pricing-cards-container">
+            <PriceCard
+              selected={true}
+              title="Free mode"
+              content="Only a limited number of reports can be generated and a limited set of features are accessible."
+              price="Free"
+              features={[
+                '- Deep dives for all metrics',
+                '- Visual Tracerouting for reports',
+                '- Manual execution',
+                '- Only 3 reports/mo',
+                '- Access to a limited number of servers',
+                <Typography key="TraceScore" variant="body1">
+                  - TraceScore&trade; isn&rsquo;t available{' '}
+                  <Chip color="default" label="🚧 In Progress" />
+                </Typography>,
+                <Typography key="CompAnal" variant="body1">
+                  - No Competitor Analysis{' '}
+                  <Chip color="default" label="🚧 In Progress" />
+                </Typography>,
+              ]}
+              link={routes.signup()}
+              linkStartIcon={<FreeBreakfast />}
+            />
+            <PriceCard
+              title="Pro mode"
+              content="Generate upto 50 reports per month, schedule it's execution and access all the features."
+              price="$15/mo"
+              features={[
+                '- Deep dives for all metrics',
+                '- Visual Tracerouting for reports',
+                '- Upto 50 reports/mo',
+                '- Schedule weekly reports',
+                '- Access to all the servers',
+                <Typography key="TraceScore" variant="TraceScore">
+                  - TraceScore&trade; is available{' '}
+                  <Chip color="default" label="🚧 In Progress" />
+                </Typography>,
+                <Typography key="CompAnal" variant="body1">
+                  - Competitor Analysis is available{' '}
+                  <Chip color="default" label="🚧 In Progress" />
+                </Typography>,
+              ]}
+              link={routes.signup()}
+              linkDisabled={true}
+              linkStartIcon={<Paid />}
+            />
+          </Box>
+        </Box>
+        <Box id="cta" component="section">
+          <Box className="cta-content-container">
+            <Box className="section-title-container">
+              <Typography variant="h2" className="section-title">
+                Optimise your applications with ease
+              </Typography>
+              <Typography variant="body1" className="section-subtitle">
+                Take the first step in optimizing your applications with
+                Tracepath. Start your monitoring journey today on our platform.
+              </Typography>
+              <Box className="hero-cta-container">
+                <Button
+                  variant="contained"
+                  component={RedwoodLink}
+                  size="large"
+                  className="primary-grad"
+                >
+                  Get Started
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+          <Box className="cta-canvas-container">
+            <canvas
+              ref={canvasRef}
+              style={{
+                width: 600,
+                height: 600,
+                maxWidth: '100%',
+              }}
+              className="cta-canvas"
+            />
+          </Box>
+        </Box>
+        <Box
+          component="footer"
+          sx={(theme) => {
+            return {
+              background: `linear-gradient(to top, ${theme.palette.secondary.dark}, ${theme.palette.secondary.main}, ${theme.palette.secondary.main})`,
+            }
+          }}
+        >
+          <Box>
+            <Box className="brand-info-container">
+              <img
+                src="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+                alt="Tracepath logo"
+                className="logo"
+              />
+              <Typography variant="body1" className="brand-name">
+                Tracepath
+              </Typography>
+            </Box>
+          </Box>
+          <Typography variant="body1" className="footer-copyright">
+            Copyright &copy; n00bgineer 2023
           </Typography>
         </Box>
-        <Box id="call-to-action" component="section">
-          <Typography variant="h2">Call to Action</Typography>
-          <Typography variant="body1">
-            Sign up for a free trial of Tracepath today and see for yourself how
-            it can help you improve the performance of your network and
-            applications.
-          </Typography>
-          <Button onClick={() => console.log('Sign Up')}>Sign Up</Button>
-        </Box>
-        <Box component="footer">
-          <Typography variant="body1">
-            Copyright &copy; 2023 Tracepath
-          </Typography>
-          <ul>
-            <li>Contact Us</li>
-            <li>Privacy Policy</li>
-            <li>Terms of Service</li>
-          </ul>
-        </Box>
-      </Box>
+      </>
     </>
   )
 }
