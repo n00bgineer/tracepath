1b8fa09f7a8901a0f01efc0807ef21ab6ef58014
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-18 04:03:30 +0530

    Updating auth page

--- web/src/pages/AuthPage/AuthPage.js
+++ web/src/pages/AuthPage/AuthPage.js
@@ -1,89 +1,84 @@
+// IMPORTING PACKAGES/MODULES
 import { useState } from 'react'
 
-import {
-  AccountCircle,
-  Email,
-  Google,
-  Lock,
-  Visibility,
-  VisibilityOff,
-} from '@mui/icons-material'
-import { Box, FormControl } from '@mui/material'
+import { AccountCircle, Login } from '@mui/icons-material'
+import { Box, Tabs, Typography } from '@mui/material'
 
 import { MetaTags } from '@redwoodjs/web'
 
-import Button from 'src/components/Button/Button'
-import IconButton from 'src/components/IconButton/IconButton'
-import Input from 'src/components/Input/Input'
+import './authPage.css'
+import Tab from 'src/components/Tab/Tab'
+
+import LoginForm from './LoginForm'
+import SignupForm from './SignupForm'
+
+/**
+ * @name a11yProps
+ * @description GENERATES TAB PROPS
+ * @param {*} index TAB INDEX
+ * @returns {Object} TAB PROPS
+ */
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  }
+}
 
 const AuthPage = () => {
-  // SETTING LOCAL STATE
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isPasswordVisible, setPasswordVisibility] = useState(false)
+  const [tabValue, setTabValue] = useState(0)
 
-  // METHODS
-  /**
-   * @name setTogglePasswordVisibility
-   * @description METHOD TO TOGGLE PASSWORD VISIBILITY
-   * @returns {undefined} undefined
-   */
-  const setTogglePasswordVisibility = () =>
-    setPasswordVisibility(!isPasswordVisible)
+  const handleChange = (event, newValue) => setTabValue(newValue)
 
   return (
     <>
       <MetaTags title="Auth" description="Auth page" />
 
-      <Box>
-        <FormControl>
-          <Button
-            size="large"
-            variant="outlined"
-            fullWidth={true}
-            startIcon={<Google />}
-            margin="large"
-            color="secondary"
-          >
-            Continue with Google
-          </Button>
-          <Input
-            placeholder="Email"
-            required={true}
-            startAdornment={<Email />}
-            fullWidth={true}
-            type="email"
-            margin="large"
-            color="secondary"
-          />
-          <Input
-            placeholder="Password"
-            required={true}
-            startAdornment={<Lock />}
-            endAdornment={
-              <IconButton>
-                {isPasswordVisible ? (
-                  <VisibilityOff onClick={setTogglePasswordVisibility} />
-                ) : (
-                  <Visibility onClick={setTogglePasswordVisibility} />
-                )}
-              </IconButton>
-            }
-            fullWidth={true}
-            type={isPasswordVisible ? 'text' : 'password'}
-            margin="large"
-            color="secondary"
-          />
-          <Button
-            size="large"
-            variant="contained"
-            fullWidth={true}
-            startAdornment={<AccountCircle />}
-            color="secondary"
-          >
-            Signup
-          </Button>
-        </FormControl>
+      <Box className="auth-page">
+        <Box className="auth-form-container">
+          <Box className="brand-info-container">
+            <img
+              src="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+              alt="Tracepath logo"
+              className="logo"
+            />
+            <Typography variant="body1" className="brand-name">
+              Tracepath
+            </Typography>
+          </Box>
+          <Box className="auth-tab-container">
+            <Tabs
+              value={tabValue}
+              onChange={handleChange}
+              variant="fullWidth"
+              sx={{
+                '&	.MuiTabs-indicator': { display: 'none' },
+                '&.MuiTabs-root': {
+                  padding: '10px',
+                  border: '1px solid #000',
+                  borderRadius: '9999px',
+                  borderColor: 'divider',
+                },
+              }}
+            >
+              <Tab
+                label="Sign up"
+                icon={<AccountCircle />}
+                iconPosition="start"
+                size="large"
+                {...a11yProps(0)}
+              />
+              <Tab
+                label="Sign in"
+                icon={<Login />}
+                iconPosition="start"
+                size="large"
+                {...a11yProps(1)}
+              />
+            </Tabs>
+          </Box>
+          {tabValue === 0 ? <SignupForm /> : <LoginForm />}
+        </Box>
       </Box>
     </>
   )
