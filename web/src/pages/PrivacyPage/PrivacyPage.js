829582f0f52cdb621ff3049ad75d12840dcb9759
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-21 01:10:26 +0530

    Adding content to ToS & Privacy page

--- web/src/pages/PrivacyPage/PrivacyPage.js
+++ web/src/pages/PrivacyPage/PrivacyPage.js
@@ -1,5 +1,6 @@
 // IMPORTING PACKAGES/MODULES
 import { Box, Typography } from '@mui/material'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 
 import { MetaTags } from '@redwoodjs/web'
 
@@ -21,6 +22,63 @@ const PrivacyPolicy = () => {
         <Typography variant="body2" className="policy-last-update">
           Last Updated On: May 20th 2023
         </Typography>
+
+        <Box className="policy-body">
+          <ReactMarkdown>
+            {`This Privacy Policy describes how n00bgineer ("us", "we", or "our") collects, uses, and discloses information when you use the Tracepath application (the "Service").
+
+## 1. Information Collection and Use
+1.1 User Data:
+
+1.1.1 We may collect certain information from users, including but not limited to:
+
+- Personal information such as name and email address when creating a user account.
+
+- Non-personal information such as IP addresses, device information, and usage data.
+
+1.1.2 We use this information to provide and improve the Service, communicate with users, and analyze usage patterns.
+
+ ### 1.2 Third-Party Services:
+
+1.2.1 The Service may use third-party services, such as analytics providers, that may collect information about your use of the Service.
+
+1.2.2 These third-party services have their own privacy policies and we recommend reviewing them to understand their data practices.
+
+## 2. Data Security
+
+2.1 We implement reasonable security measures to protect user data against unauthorized access or disclosure.
+
+2.2 However, no method of transmission or storage is completely secure, and we cannot guarantee the absolute security of user data.
+
+## 3. Data Retention
+
+3.1 We retain user data for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.
+
+3.2 Users may request the deletion of their data by contacting us at **n00bgineer@protonmail.ch**.
+
+## 4. Disclosure of Information
+4.1 We may disclose user data in the following circumstances:
+
+- To comply with legal obligations or respond to lawful requests.
+
+- To protect our rights, property, or safety, or the rights, property, or safety of others.
+
+- With the user's consent.
+
+## 5. Children's Privacy
+5.1 The Service is not intended for use by individuals under the age of 13. We do not knowingly collect personal information from children under 13 years of age. If we become aware that we have collected personal information from a child under the age of 13, we will take steps to delete the information as soon as possible.
+
+## 6. Changes to this Privacy Policy
+
+6.1 We may update this Privacy Policy from time to time. The most current version will be posted on our website or provided within the Service.
+
+6.2 It is your responsibility to review this Privacy Policy periodically for any changes. Your continued use of the Service after any modifications to this Privacy Policy constitutes your acceptance of such changes.
+
+## 7. Contact Us
+If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at **n00bgineer@protonmail.ch**.
+`}
+          </ReactMarkdown>
+        </Box>
       </Box>
       <Footer />
     </>
--- web/src/pages/ToSPage/ToSPage.js
+++ web/src/pages/ToSPage/ToSPage.js
@@ -1,5 +1,6 @@
 // IMPORTING PACKAGES/MODULES
 import { Box, Typography } from '@mui/material'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 
 import { MetaTags } from '@redwoodjs/web'
 
@@ -21,6 +22,64 @@ const ToSPage = () => {
         <Typography variant="body2" className="policy-last-update">
           Last Updated On: May 20th 2023
         </Typography>
+        <Box className="policy-body">
+          <ReactMarkdown>
+            {`Please read these Terms of Service ("Terms", "Terms of Service") carefully before using the Tracepath application (the "Service") operated by n00bgineer ("us", "we", or "our").
+
+## 1. Acceptance of Terms
+By accessing or using the Service, you agree to be bound by these Terms. If you disagree with any part of the terms, you may not access the Service.
+
+## 2. Description of Service
+Tracepath is an open-source application that provides users with simplified reports about the performance of their applications. The Service allows users to track, analyze, and improve performance indicators of their applications through simplified metrics, visual tracerouting, and generating comprehensive reports across global locations.
+
+## 3. Use of the Service
+### 3.1 User Accounts:
+3.1.1 In order to access certain features of the Service, you may be required to create a user account. You must provide accurate and complete information when creating an account.
+
+3.1.2 You are solely responsible for maintaining the confidentiality of your account and password. You agree to notify us immediately of any unauthorized use of your account.
+
+### 3.2 Acceptable Use:
+3.2.1 You agree to use the Service in compliance with all applicable laws and regulations.
+
+3.2.2 You shall not use the Service to engage in any illegal, abusive, or unauthorized activities.
+
+### 3.3 Intellectual Property:
+
+3.3.1 The Service and its original content, features, and functionality are and will remain the exclusive property of n00bgineer and its licensors.
+
+3.3.2 You may not copy, modify, distribute, sell, or lease any part of the Service or its content without prior written permission from n00bgineer.
+
+## 4. Privacy
+### 4.1 Data Collection:
+
+4.1.1 We may collect certain information from users as described in our Privacy Policy. By using the Service, you consent to the collection and use of your data in accordance with our Privacy Policy.
+
+### 4.2 Security:
+
+4.2.1 We strive to implement reasonable security measures to protect the confidentiality and integrity of user data. However, no method of transmission or electronic storage is completely secure, and we cannot guarantee absolute security.
+
+## 5. Limitation of Liability
+5.1 You expressly understand and agree that n00bgineer shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses resulting from the use or inability to use the Service.
+
+## 6. Modification and Termination
+6.1 We reserve the right to modify, suspend, or discontinue the Service at any time without prior notice or liability.
+
+6.2 We may also terminate or suspend your access to the Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach these Terms.
+
+## 7. Governing Law
+7.1 These Terms shall be governed and construed in accordance with the laws of India, without regard to its conflict of law provisions.
+
+## 8. Severability
+8.1 If any provision of these Terms is found to be unenforceable or invalid under any applicable law, such unenforceability or invalidity shall not render these Terms unenforceable or invalid as a whole.
+
+## 9. Entire Agreement
+9.1 These Terms constitute the entire agreement between you and n00bgineer regarding the use of the Service and supersede any prior agreements or understandings.
+
+## 10. Contact Us
+10.1 If you have any questions about these Terms, please contact us at n00bgineer@protonmail.ch.
+            `}
+          </ReactMarkdown>
+        </Box>
       </Box>
       <Footer />
     </>
