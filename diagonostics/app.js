c8efe192d8701b5a75502557cb2f94d61be3ccc5
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-22 14:40:18 +0530

    Diagonostics server basic setup

---
+++ diagonostics/app.js
@@ -0,0 +1,19 @@
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
