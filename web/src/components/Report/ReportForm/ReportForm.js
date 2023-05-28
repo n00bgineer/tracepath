4596d400ca43f509aa3876eea88403d9752f53b2
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-29 03:10:37 +0530

    Updating report form

--- web/src/components/Report/ReportForm/ReportForm.js
+++ web/src/components/Report/ReportForm/ReportForm.js
@@ -1,38 +1,129 @@
 // IMPORTING PACKAGES/MODULES
-import { Link, Storage } from '@mui/icons-material'
-import { Box, Typography } from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
+
+import { useApolloClient } from '@apollo/client'
+import { Assessment, Link, Storage } from '@mui/icons-material'
+import { Box, Skeleton, Typography } from '@mui/material'
 import './reportForm.css'
 import Globe from 'react-globe.gl'
+import { useRecoilState } from 'recoil'
 
+import Alert from 'src/components/Alert/Alert'
 import Button from 'src/components/Button/Button'
 import Input from 'src/components/Input/Input'
+import { QUERY as REGIONS_QUERY } from 'src/components/Region/RegionsCell/RegionsCell'
 import Select from 'src/components/Select/Select'
+import { darkThemeAtom } from 'src/contexts/atoms'
 
-const ReportForm = (props) => {
+/**
+ * @name validateUrl
+ * @description METHOD TO VALIDATE URL
+ * @param {*} url URL VALUE
+ * @returns {Boolean} WHETHER URL IS CORRECT OR NOT
+ */
+const validateUrl = (url) => {
+  const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/\S*)?$/
+  return urlPattern.test(url)
+}
+
+const ReportForm = ({ loading, onSave, error, report }) => {
+  // SETTING LOCAL STATE
+  const [url, setUrl] = useState('')
+  const [selectItems, setSelectItems] = useState(null)
+  const [selectedRegion, setSelectedRegion] = useState(null)
+  const [hasRegionsDataLoaded, setHasRegionsDataLoaded] = useState(false)
+
+  // INPUT FIELD VALIDATION STATES
+  const [urlErrorText, setUrlErrorText] = useState('')
+  const [submitErrorText, setSubmitErrorText] = useState('')
+
+  // GETTING ATOMIC STATES
+  const [isDarkMode] = useRecoilState(darkThemeAtom)
+
+  // INITIALISING APOLLO CLIENT
+  const client = useApolloClient()
+
+  // SETTING REF
+  const globeContainerRef = useRef(null)
+
   // METHODS
-  const onSubmit = (data) => {
-    props.onSave(data, props?.report?.id)
+  /**
+   * @name setUrlField
+   * @description METHOD TO SET EMAIL VALUE
+   * @returns {undefined} undefined
+   */
+  const setUrlField = (event) => {
+    setUrl(event.target.value)
+
+    if (!validateUrl(event.target.value.trim()))
+      setUrlErrorText(
+        'URL format is incorrect (e.g. espn.com or https://espn.com)'
+      )
+    else setUrlErrorText('')
   }
 
-  // SETTING LOCAL VARIABLES
-  const selectItems = [
-    {
-      label: '🇺🇸 Virginia',
-      value: 'us-east-1a',
-    },
-    {
-      label: '🇺🇸 Oregon',
-      value: 'us-west-2a',
-    },
-    {
-      label: '🇮🇳 India',
-      value: 'ap-south-1a',
-    },
-    {
-      label: '🇸🇪 Stockholm',
-      value: 'eu-north-1a',
-    },
-  ]
+  /**
+   * @name setSelectedField
+   * @description METHOD TO SET SELECTED FIELD
+   * @param {*} event EVENT OBJECT
+   * @returns {undefined} undefined
+   */
+  const setSelectedField = (event) => setSelectedRegion(event.target.value)
+
+  /**
+   * @name onSubmit
+   * @description METHOD TO SUBMIT DATA
+   * @param {*} data DATA TO BE SUBMITED
+   * @returns {undefined} undefined
+   */
+  const onSubmit = (data) => {
+    if (selectedRegion === null || selectedRegion === 'default')
+      setSubmitErrorText('Please select a region to continue')
+    else if (urlErrorText !== '' || url.length === 0)
+      setSubmitErrorText('Please enter an appropriate URL')
+    else {
+      if (error) {
+        setSubmitErrorText(error.message)
+      } else {
+        setSubmitErrorText('')
+      }
+      onSave(data, report?.id)
+    }
+  }
+
+  /**
+   * @name setRegionsLoad
+   * @description METHOD TO LOAD REGIONS DATA
+   * @returns {undefined} undefined
+   */
+  const setRegionsLoad = async () => {
+    await client
+      .query({
+        query: REGIONS_QUERY,
+      })
+      .then((res) => {
+        setSelectItems(
+          res.data.regions.map((region) => {
+            return {
+              label: region.name,
+              value: region.regionName,
+              chipLabel: region.status === 'OK' ? '🟢 ONLINE' : '🔴 OFFLINE',
+              chipColor: region.status === 'OK' ? 'primary' : 'error',
+            }
+          })
+        )
+        setHasRegionsDataLoaded(true)
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }
+
+  // SETTING SIDE EFFECTS
+  useEffect(() => {
+    setRegionsLoad()
+  }, [])
+
   return (
     <Box className="report-form-globe-container">
       <Box
@@ -50,50 +141,67 @@ const ReportForm = (props) => {
         </Typography>
         <Typography variant="body2" className="report-form-subtitle">
           Gain valuable insights for optimizing your application&rsquo;s
-          performance through our comprehensive reports
+          performance and security through our comprehensive reports
         </Typography>
-        <form>
-          <Select
-            fullWidth={true}
-            margin="medium"
-            label="📍 Select Server Location"
-            placeholder="📍 Select Server Location"
-            className="select-server"
-            size="small"
-            defaultValue="default"
-            startAdornment={<Storage />}
-            selectItems={selectItems}
-          />
-          <Input
-            placeholder="Enter URL"
-            required={true}
-            startAdornment={<Link />}
-            fullWidth={true}
-            type="url"
-            value={''}
-            margin="medium"
-            color={false === true ? 'error' : 'primary'}
-            onInput={() => {}}
-            errorText={''}
-            label="email"
-            size="small"
-          />
-          <Button
-            margin="large"
-            fullWidth={true}
-            variant="contained"
-            size="small"
-            onClick={onSubmit}
-          >
-            Generate Report
-          </Button>
-        </form>
+        {hasRegionsDataLoaded ? (
+          <form>
+            <Select
+              fullWidth={true}
+              margin="medium"
+              label="📍 Select Server Location"
+              placeholder="📍 Select Server Location"
+              className="select-server"
+              size="small"
+              defaultValue="default"
+              startAdornment={<Storage />}
+              selectItems={selectItems}
+              onChange={setSelectedField}
+            />
+            <Input
+              placeholder="Enter URL"
+              required={true}
+              startAdornment={<Link />}
+              fullWidth={true}
+              type="url"
+              value={url}
+              margin="medium"
+              color={urlErrorText !== '' ? 'error' : 'primary'}
+              onInput={setUrlField}
+              errorText={urlErrorText}
+              label="url"
+              size="small"
+            />
+            <Button
+              type="submit"
+              margin="medium"
+              fullWidth={true}
+              variant="contained"
+              size="small"
+              startIcon={<Assessment />}
+              onClick={onSubmit}
+              disabled={loading}
+            >
+              Get Insights
+            </Button>
+            {submitErrorText !== '' && (
+              <Alert fullWidth={true} margin="large" severity="error">
+                {submitErrorText}
+              </Alert>
+            )}
+          </form>
+        ) : (
+          <Skeleton variant="rectangular" className="report-form-skeleton" />
+        )}
       </Box>
-      <div className="report-globe-container">
+      <div className="report-globe-container" ref={globeContainerRef}>
         <Globe
-          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-          zoom={5}
-          bearing={1}
+          globeImageUrl={
+            isDarkMode
+              ? '//unpkg.com/three-globe/example/img/earth-night.jpg'
+              : '//unpkg.com/three-globe/example/img/earth-day.jpg'
+          }
+          width={globeContainerRef.current?.offsetWidth}
+          height={globeContainerRef.current?.offsetHeight}
         />
       </div>
     </Box>
