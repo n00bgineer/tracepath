7ce365cab42573b8e746cd0c85af96da4e534eaa
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-28 19:51:14 +0530

    Removing everything from return body except <ReportForm />

--- web/src/components/Report/NewReport/NewReport.js
+++ web/src/components/Report/NewReport/NewReport.js
@@ -30,16 +30,7 @@ const NewReport = () => {
     createReport({ variables: { input } })
   }
 
-  return (
-    <div className="rw-segment">
-      <header className="rw-segment-header">
-        <h2 className="rw-heading rw-heading-secondary">New Report</h2>
-      </header>
-      <div className="rw-segment-main">
-        <ReportForm onSave={onSave} loading={loading} error={error} />
-      </div>
-    </div>
-  )
+  return <ReportForm onSave={onSave} loading={loading} error={error} />
 }
 
 export default NewReport
