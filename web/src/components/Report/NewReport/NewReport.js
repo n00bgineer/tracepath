f7496819237d341226906267a7c3e9e3e5981b58
Aashish Panigrahi <asxyzp@gmail.com>
2023-05-29 18:35:35 +0530

    Fetching more data

--- web/src/components/Report/NewReport/NewReport.js
+++ web/src/components/Report/NewReport/NewReport.js
@@ -1,6 +1,5 @@
-import { navigate, routes } from '@redwoodjs/router'
+// IMPORTING PACKAGES/MODULES
 import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
 
 import ReportForm from 'src/components/Report/ReportForm'
 
@@ -8,6 +7,38 @@ const CREATE_REPORT_MUTATION = gql`
   mutation CreateReportMutation($input: CreateReportInput!) {
     createReport(input: $input) {
       id
+      userId
+      createdAt
+      updateAt
+      reportVersion
+      lhVersion
+      executionTime
+      isPrivate
+      isTracerouteError
+      isLighthouseError
+      regionName
+      url
+      finalUrl
+      traceroute
+      fcpScore
+      fcpValue
+      lcpScore
+      lcpValue
+      tbtScore
+      tbtValue
+      ttiScore
+      ttiValue
+      clsScore
+      srtValue
+      srtItems
+      speedIndexScore
+      speedIndexValue
+      bootupTimeScore
+      bootupTimeValue
+      bootupTimeItems
+      bootupTimeSummary
+      thirdPartyItems
+      thirdPartySummary
     }
   }
 `
@@ -16,18 +47,13 @@ const NewReport = () => {
   const [createReport, { loading, error }] = useMutation(
     CREATE_REPORT_MUTATION,
     {
-      onCompleted: () => {
-        toast.success('Report created')
-        navigate(routes.reports())
-      },
-      onError: (error) => {
-        toast.error(error.message)
-      },
+      onCompleted: () => console.log('REPORT CREATED'),
+      onError: (error) => console.error(error.message),
     }
   )
 
-  const onSave = (input) => {
-    createReport({ variables: { input } })
+  const onSave = async (input) => {
+    return await createReport({ variables: { input } })
   }
 
   return <ReportForm onSave={onSave} loading={loading} error={error} />
