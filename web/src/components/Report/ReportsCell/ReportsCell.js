0b14ac4edb5dcdbee162cb0572fb0aefb88a02a4
Aashish Panigrahi <asxyzp@gmail.com>
2023-06-02 00:19:07 +0530

    Updating state components of Reports cell

--- web/src/components/Report/ReportsCell/ReportsCell.js
+++ web/src/components/Report/ReportsCell/ReportsCell.js
@@ -1,38 +1,23 @@
 // IMPORTING PACKAGES/MODULES
-import { Link, routes } from '@redwoodjs/router'
+import { routes } from '@redwoodjs/router'
 
 import Reports from 'src/components/Report/Reports'
+import ScreenLoading from 'src/components/ScreenLoading/ScreenLoading'
 
 export const QUERY = gql`
   query FindReports {
     reports {
       id
-      reportVersion
-      lhVersion
-      executionTime
-      regionName
       url
+      createdAt
       finalUrl
-      traceroute
       fcpScore
-      fcpValue
       lcpScore
-      lcpValue
       tbtScore
-      tbtValue
       ttiScore
-      ttiValue
       clsScore
-      srtValue
-      srtItems
       speedIndexScore
-      speedIndexValue
       bootupTimeScore
-      bootupTimeValue
-      bootupTimeItems
-      bootupTimeSummary
-      thirdPartyItems
-      thirdPartySummary
       siteMeta
       User {
         userName
@@ -44,22 +29,40 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading...</div>
-
-export const Empty = () => {
+export const Loading = () => {
   return (
-    <div className="rw-text-center">
-      {'No reports yet. '}
-      <Link to={routes.generate()} className="rw-link">
-        {'Create one?'}
-      </Link>
-    </div>
+    <ScreenLoading
+      imgUrl="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+      title="Fetching Curated Reports"
+      subtitle="Discovering curated performance and security insights of various web apps"
+    />
   )
 }
 
-export const Failure = ({ error }) => (
-  <div className="rw-cell-error">{error?.message}</div>
-)
+export const Empty = () => {
+  return (
+    <ScreenLoading
+      imgUrl="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+      title="No curated reports"
+      subtitle="Currently, there are no curated performance and security reports to showcase"
+      btnLink={routes.generate()}
+      btnText="Generate Report"
+    />
+  )
+}
+
+export const Failure = ({ error }) => {
+  console.log(error.message)
+  return (
+    <ScreenLoading
+      imgUrl="https://res.cloudinary.com/dgu9rv3om/image/upload/v1683873993/tracepath/assets/logo1-cropped_hcmo16.png"
+      title="Something went wrong"
+      subtitle="If the problem persists, please contact our support team for further assistance"
+      errorLink={routes.generate()}
+      errorText="Go back"
+    />
+  )
+}
 
 export const Success = ({ reports }) => {
   return <Reports reports={reports} />
