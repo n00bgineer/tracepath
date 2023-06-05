2279987daa0bf052c6ac200e2a4addc5f09514ec
Aashish Panigrahi <asxyzp@gmail.com>
2023-06-06 02:00:42 +0530

    Making dark theme the default state

--- web/src/contexts/atoms.js
+++ web/src/contexts/atoms.js
@@ -4,7 +4,7 @@ import { atom } from 'recoil'
 // STORING MODAL TYPE
 export const darkThemeAtom = atom({
   key: 'darkTheme',
-  default: false,
+  default: true,
 })
 export const reportAtom = atom({
   key: 'recentReport',
