f2466cf48a82c078e183e3db6ed8073a46c68d35
Aashish Panigrahi <asxyzp@gmail.com>
2023-06-07 02:45:17 +0530

    Adding create user and update user mutations

--- web/src/contexts/context.js
+++ web/src/contexts/context.js
@@ -1,6 +1,15 @@
 // IMPORTING PACKAGES/MODULES
 import React, { createContext, useContext } from 'react'
 
+import { useRecoilState } from 'recoil'
+
+import { useMutation } from '@redwoodjs/web'
+
+import { UPDATE_USER_MUTATION } from 'src/components/User/EditUserCell'
+import { CREATE_USER_MUTATION } from 'src/components/User/NewUser'
+
+import { accountAtom } from './atoms'
+
 // CREATING GLOBAL CONTEXT
 const AppContext = createContext()
 
@@ -22,10 +31,60 @@ export const useAppContext = () => {
  * @returns <AppContextProvider/> (JSX)
  */
 const Context = ({ children }) => {
-  const signinUser = async (id, input) => {}
-  const signupUser = async (input) => {}
+  // GETTING ATOMIC STATES
+  const [account, setAccount] = useRecoilState(accountAtom)
+
+  // EXECUTING GQL MUTATION
+  // MUTATION FOR CREATING NEW USER
+  const [createUser] = useMutation(CREATE_USER_MUTATION, {
+    // ON SUCCESSFUL EXECUTION OF MUTATION
+    onCompleted: (res) => {
+      // UPDATING USER STATE
+      setAccount(res.createUser)
+    },
+    // WHEN AN ERROR HAS OCCURED WHILE EXECUTION OF MUTATION
+    onError: (error) => {
+      console.error('ERROR OCCURED WHILE SIGNING UP')
+      console.error(error)
+    },
+  })
+
+  // MUTATION FOR UPDATING NEW USER
+  const [updateUser] = useMutation(UPDATE_USER_MUTATION, {
+    onCompleted: (res) => {
+      // UPDATING USER STATE
+      setAccount(res.createUser)
+    },
+    onError: (error) => {
+      console.error('ERROR OCCURED WHILE SIGNING IN')
+      console.error(error)
+    },
+  })
+
+  // METHODS
+  /**
+   * @name signinUser
+   * @description METHOD TO SIGNIN
+   * @param {*} id USER GUID
+   * @param {*} input FIELDS TO BE UPDATED
+   */
+  const signinUser = async (id, input) => {
+    await createUser({ variables: { input } })
+  }
+
+  /**
+   * @name signupUser
+   * @description METHOD TO SIGNUP
+   * @param {*} id USER ID
+   * @param {*} input INPUT FOT SIGNING UP
+   */
+  const signupUser = async (id, input) => {
+    await updateUser({ variables: { id, input } })
+  }
 
   const ContextVal = {
+    account: account,
+    setAccount: setAccount,
     signinUser: signinUser,
     signupUser: signupUser,
   }
