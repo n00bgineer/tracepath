05bbdd5d6b9871fc153f01aadabcd7ea1ecc048a
Aashish Panigrahi <asxyzp@gmail.com>
2023-06-06 19:19:45 +0530

    Updating user page

--- web/src/components/User/User/User.js
+++ web/src/components/User/User/User.js
@@ -1,107 +1,199 @@
-import { Link, routes, navigate } from '@redwoodjs/router'
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
+// IMPORTING PACKAGES/MODULES
+import { useEffect, useState } from 'react'
 
-import { formatEnum, timeTag } from 'src/lib/formatters'
+import { useApolloClient } from '@apollo/client'
+import { Assessment, Inventory2 } from '@mui/icons-material'
+import { Box, Skeleton, Tabs, Typography, useMediaQuery } from '@mui/material'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import './user.css'
+import 'src/components/Report/Reports/reports.css'
+import { useRecoilState } from 'recoil'
 
-const DELETE_USER_MUTATION = gql`
-  mutation DeleteUserMutation($id: String!) {
-    deleteUser(id: $id) {
-      id
-    }
-  }
-`
+import { Link as RedwoodLink, routes } from '@redwoodjs/router'
+
+import DataLoading from 'src/components/DataLoading/DataLoading'
+import { capitalise } from 'src/components/HopTimeline/HopTimeline'
+import { QUERY1 as USER_REPORTS_QUERY } from 'src/components/Report/ReportsCell'
+import SiteMetaCard from 'src/components/SiteMetaCard/SiteMetaCard'
+import Tab from 'src/components/Tab/Tab'
+import TabPanel from 'src/components/TabPanel/TabPanel'
+import { userReportsAtom } from 'src/contexts/atoms'
+
+// INSTANTIATING TIME AGO DEFAULT LOCALE
+TimeAgo.addDefaultLocale(en)
+const timeAgo = new TimeAgo('en-US')
 
 const User = ({ user }) => {
-  const [deleteUser] = useMutation(DELETE_USER_MUTATION, {
-    onCompleted: () => {
-      toast.success('User deleted')
-      navigate(routes.users())
-    },
-    onError: (error) => {
-      toast.error(error.message)
-    },
-  })
+  // SETTING LOCAL STATES
+  const [tabValue, setTabValue] = useState(0)
 
-  const onDeleteClick = (id) => {
-    if (confirm('Are you sure you want to delete user ' + id + '?')) {
-      deleteUser({ variables: { id } })
-    }
+  // SETTING MEDIA QUERY
+  const isMobileViewport = useMediaQuery('(max-width:900px)')
+
+  // GETTING ATOMIC STATES
+  const [reports, setReports] = useRecoilState(userReportsAtom)
+
+  // INITIALISING APOLLO CLIENT
+  const client = useApolloClient()
+
+  // METHODS
+  /**
+   * @name setTab
+   * @description METHOD TO SET TAB VALUE
+   * @param {*} event EVENT OBJECT
+   * @param {*} value VALUE
+   * @returns {undefined} undefined
+   */
+  const setTab = (event, value) => setTabValue(value)
+
+  /**
+   * @name setUserReports
+   * @description METHOD TO LOAD USER REPORTS DATA
+   * @returns {undefined} undefined
+   */
+  const setUserReports = async () => {
+    await client
+      .query({
+        query: USER_REPORTS_QUERY,
+        variables: {
+          id: 'bba8c3b0-fc14-43b0-a2fb-4ab8a8314d46',
+        },
+      })
+      .then((res) => {
+        setReports(res.data.userReports)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
+  useEffect(() => {
+    if (reports === null || reports === undefined) setUserReports()
+    console.log(reports)
+  }, [reports])
+
   return (
-    <>
-      <div className="rw-segment">
-        <header className="rw-segment-header">
-          <h2 className="rw-heading rw-heading-secondary">
-            User {user.id} Detail
-          </h2>
-        </header>
-        <table className="rw-table">
-          <tbody>
-            <tr>
-              <th>Id</th>
-              <td>{user.id}</td>
-            </tr>
-            <tr>
-              <th>Guid</th>
-              <td>{user.guid}</td>
-            </tr>
-            <tr>
-              <th>Created at</th>
-              <td>{timeTag(user.createdAt)}</td>
-            </tr>
-            <tr>
-              <th>Update at</th>
-              <td>{timeTag(user.updateAt)}</td>
-            </tr>
-            <tr>
-              <th>Last login at</th>
-              <td>{timeTag(user.lastLoginAt)}</td>
-            </tr>
-            <tr>
-              <th>Role type</th>
-              <td>{formatEnum(user.roleType)}</td>
-            </tr>
-            <tr>
-              <th>Account type</th>
-              <td>{formatEnum(user.accountType)}</td>
-            </tr>
-            <tr>
-              <th>Subscription type</th>
-              <td>{formatEnum(user.subscriptionType)}</td>
-            </tr>
-            <tr>
-              <th>User name</th>
-              <td>{user.userName}</td>
-            </tr>
-            <tr>
-              <th>Email</th>
-              <td>{user.email}</td>
-            </tr>
-            <tr>
-              <th>Display name</th>
-              <td>{user.displayName}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <nav className="rw-button-group">
-        <Link
-          to={routes.editUser({ id: user.id })}
-          className="rw-button rw-button-blue"
+    <Box className="user-account-container">
+      <Box className="user-account-header">
+        <Box
+          className="user-account-header-image-container"
+          sx={(theme) => {
+            return { border: `1px solid ${theme.palette.divider}` }
+          }}
         >
-          Edit
-        </Link>
-        <button
-          type="button"
-          className="rw-button rw-button-red"
-          onClick={() => onDeleteClick(user.id)}
+          <img
+            src="https://res.cloudinary.com/dgu9rv3om/image/upload/q_auto:low/v1685999029/An_illustration_of_lush_gren_mountain_forest_again-transformed_naqdxt.jpg"
+            className="user-header-img"
+            alt="Lush green mountains"
+          />
+        </Box>
+        <Box className="user-avatar-info-container">
+          <img
+            src={`https://api.dicebear.com/6.x/avataaars-neutral/svg?seed=${user.userName}`}
+            className="user-avatar-img"
+            alt="User Account Avatar"
+          />
+          <Box className="user-info-container">
+            <Typography variant="h5" className="user-display-name">
+              {user.displayName}
+            </Typography>
+            <Typography variant="body2" color="grey">
+              @{user.userName} / {capitalise(user.accountType)}
+            </Typography>
+            <Typography variant="body2" color="grey">
+              Joined {timeAgo.format(new Date(user.createdAt))}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box className="user-account-body">
+        <Tabs
+          value={tabValue}
+          onChange={setTab}
+          TabIndicatorProps={{
+            sx: { display: 'none' },
+          }}
+          centered={isMobileViewport}
         >
-          Delete
-        </button>
-      </nav>
-    </>
+          <Tab
+            label="Your reports"
+            icon={<Assessment fontSize="small" />}
+            iconPosition="start"
+          />
+          <Tab
+            label={'Your projects'}
+            icon={<Inventory2 fontSize="small" />}
+            iconPosition="start"
+            disabled
+          />
+        </Tabs>
+        <TabPanel value={0} className="user-tab" index={tabValue}>
+          {/* LOADING STATE */}
+          {reports === null && (
+            <Skeleton variant="rectangular" className="user-data-skeleton" />
+          )}
+
+          {/*  */}
+          {reports === undefined && (
+            <DataLoading
+              title="Reports Error"
+              subtitle="An error occured while loading your report"
+              gray={true}
+              imgUrl="https://res.cloudinary.com/dgu9rv3om/image/upload/q_auto:low/v1685743033/emptyPerformance_rbqkus.png"
+            />
+          )}
+          {reports && reports.length == 0 && (
+            <DataLoading
+              title="No Reports"
+              subtitle="Couldn't find any reports for your account"
+              gray={true}
+              imgUrl="https://res.cloudinary.com/dgu9rv3om/image/upload/v1686058485/empty-user-reports_xbcf3b.png"
+            />
+          )}
+          {reports && reports.length > 0 && (
+            <Box className="explore-content-container">
+              {reports
+                .filter((report) => {
+                  if (
+                    report.siteMeta.ogTitle ||
+                    report.siteMeta.ogDescription ||
+                    report.siteMeta.twitterDescription ||
+                    report.siteMeta.description
+                  )
+                    return true
+                })
+                .map((report, index) => {
+                  // SETTING LOCAL VARIABLES
+                  const scores = {
+                    fcp: report.fcpScore,
+                    lcp: report.lcpScore,
+                    cls: report.clsScore,
+                    tbt: report.tbtScore,
+                    tti: report.ttiScore,
+                    speedIndex: report.speedIndexScore,
+                  }
+
+                  return (
+                    <SiteMetaCard
+                      component={RedwoodLink}
+                      to={routes.report({ id: report.id })}
+                      className="reports-card"
+                      key={index}
+                      url={report.url}
+                      finalUrl={report.finalUrl}
+                      siteMeta={report.siteMeta}
+                      Region={report.Region}
+                      createdAt={report.createdAt}
+                      scores={scores}
+                    />
+                  )
+                })}
+            </Box>
+          )}
+        </TabPanel>
+      </Box>
+    </Box>
   )
 }
 
