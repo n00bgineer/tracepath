9a7d728a335789482c9993e272dae9e44f1a151b
Aashish Panigrahi <asxyzp@gmail.com>
2023-06-07 02:44:53 +0530

    Storing global context values on client-side

--- web/src/contexts/atoms.js
+++ web/src/contexts/atoms.js
@@ -1,9 +1,15 @@
 // IMPORTING PACKAGES/MODULES
 import { atom } from 'recoil'
 
+import global from './global'
+
 // STORING MODAL TYPE
 export const reportAtom = atom({
   key: 'recentReport',
+  default: null,
+})
+export const accountAtom = atom({
+  key: 'account',
   default: null,
 })
 export const regionsAtom = atom({
@@ -12,7 +18,7 @@ export const regionsAtom = atom({
 })
 export const darkThemeAtom = atom({
   key: 'darkTheme',
-  default: false,
+  default: global.isDarkTheme,
 })
 export const userReportsAtom = atom({
   key: 'userReports',
---
+++ web/src/contexts/global.js
@@ -0,0 +1,3 @@
+export default {
+  isDarkTheme: true,
+}
