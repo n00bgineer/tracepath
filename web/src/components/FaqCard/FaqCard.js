81a795acc4fb0b5a909fa9f07fbd235a1597361f
n00bgineer <n00bgineer@protonmail.com>
2023-07-01 22:47:28 +0530

    Review: Simplifying card styling

--- web/src/components/FaqCard/FaqCard.js
+++ web/src/components/FaqCard/FaqCard.js
@@ -1,27 +1,46 @@
-import { Card, Typography } from '@mui/material'
+// IMPORTING PACKAGES/MODULES
+import { Card as MuiCard, Typography, styled } from '@mui/material'
 import './faqCard.css'
+
+// CUSTOM COMPONENTS
+// CUSTOM CARD COMPONENT
+const CustomCard = styled(MuiCard)(({ theme }) => ({
+  '&.faq-card': {
+    border: '1px solid',
+    borderRadius: '25px',
+    display: 'flex',
+    flexGrow: '1',
+    flexWrap: 'wrap',
+    margin: '15px',
+    maxWidth: 'calc(33% - 50px)',
+    minWidth: '400px',
+    padding: '25px',
+    borderColor:
+      theme.palette.mode === 'light'
+        ? 'hsl(214.3 31.8% 91.4)'
+        : 'rgba(230,230,230,0.4)',
+    backdropFilter: 'blur(10px)',
+  },
+  '&.faq-card > .faq-question': {
+    fontSize: '1.1em',
+    fontWeight: 'bold',
+  },
+  '&.faq-card > .faq-answer': {
+    fontSize: '0.9em',
+    marginTop: '5px',
+  },
+}))
 
 const FaqCard = ({ question, answer }) => {
   return (
-    <Card
-      className="faq-card"
-      sx={(theme) => {
-        return {
-          borderColor:
-            theme.palette.mode === 'light'
-              ? 'hsl(214.3 31.8% 91.4)'
-              : 'rgba(230,230,230,0.4)',
-          backdropFilter: 'blur(10px)',
-        }
-      }}
-    >
+    <CustomCard className="faq-card">
       <Typography className="faq-question" variant="h6">
         {question}
       </Typography>
       <Typography className="faq-answer" variant="body1">
         {answer}
       </Typography>
-    </Card>
+    </CustomCard>
   )
 }
 
