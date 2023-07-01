e0906e59bc997f58040ec70017f7d47f14573ec9
n00bgineer <n00bgineer@protonmail.com>
2023-07-01 22:36:33 +0530

    Review: Removing gray prop, simplifying classname value, using custom component and moving most style classes to custom components

--- web/src/components/DataLoading/DataLoading.js
+++ web/src/components/DataLoading/DataLoading.js
@@ -1,35 +1,56 @@
 // IMPORTING PACKAGES/MODULES
 import React from 'react'
 
-import { Box, Typography } from '@mui/material'
+import { Box as MuiBox, Typography, styled } from '@mui/material'
 import './dataLoading.css'
 
-const DataLoading = ({ imgUrl, title, subtitle, small, gray, ...props }) => {
+// CUSTOM COMPONENTS
+// CUSTOM BOX COMPONENT
+const CustomBox = styled(MuiBox)(({ theme }) => ({
+  '&.MuiBox-root': {
+    background:
+      theme.palette.mode === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.background.paper,
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: '25px',
+    padding: '10px',
+    margin: '0px auto 10px',
+  },
+  '&.data-loading-container > .data-loading-container-image': {
+    maxHeight: '300px',
+    minHeight: '300px',
+    margin: '10px auto 0px',
+    display: 'block',
+  },
+  '& .loading-text-container': {
+    width: '75%',
+    margin: '15px auto 10px',
+  },
+  '& .data-loading-container-title': {
+    fontSize: '1.25em',
+    fontWeight: '900',
+    textAlign: 'center',
+  },
+  '& .data-loading-container-subtitle': {
+    fontSize: '0.85em',
+    textAlign: 'center',
+    marginTop: '0px',
+  },
+}))
+
+const DataLoading = ({ imgUrl, title, subtitle, small, ...props }) => {
   // SETTING LOCAL VARIABLES
   // SETTING LOADING CONTAINER SIZE
   let loadingContainerSizeClass = 'data-loading-container-large'
   if (small === true) loadingContainerSizeClass = 'data-loading-container-small'
 
   return (
-    <Box
-      sx={(theme) => {
-        return {
-          bgcolor:
-            theme.palette.mode === 'light'
-              ? gray
-                ? 'grey.200'
-                : 'background.default'
-              : 'background.paper',
-          border: `1px solid ${theme.palette.divider}`,
-        }
-      }}
-      className={
-        props.className
-          ? props.className +
-            ' data-loading-container ' +
-            loadingContainerSizeClass
-          : 'data-loading-container ' + loadingContainerSizeClass
-      }
+    <CustomBox
+      {...props}
+      className={`${
+        props.className ? props.className : ''
+      } data-loading-container ${loadingContainerSizeClass}`}
     >
       {imgUrl && (
         <img
@@ -39,15 +60,15 @@ const DataLoading = ({ imgUrl, title, subtitle, small, gray, ...props }) => {
           loading="lazy"
         />
       )}
-      <Box className="loading-text-container">
+      <MuiBox className="loading-text-container">
         <Typography variant="h2" className="data-loading-container-title">
           {title}
         </Typography>
         <Typography variant="body1" className="data-loading-container-subtitle">
           {subtitle}
         </Typography>
-      </Box>
-    </Box>
+      </MuiBox>
+    </CustomBox>
   )
 }
 
