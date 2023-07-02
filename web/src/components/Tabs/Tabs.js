5d428020bc8f2504a8c4b6ec816716821a3635c5
n00bgineer <n00bgineer@protonmail.com>
2023-07-03 01:42:08 +0530

    Creating tabs component

---
+++ web/src/components/Tabs/Tabs.js
@@ -0,0 +1,13 @@
+// IMPORTING PACKAGES/MODULES
+import { Tabs as MuiTabs } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+const Tabs = styled(MuiTabs)(() => ({
+  '&.MuiTabs-root': {
+    padding: '10px',
+    background: 'rgba(230,230,230,0.2)',
+    borderRadius: '9999px',
+  },
+}))
+
+export default Tabs
