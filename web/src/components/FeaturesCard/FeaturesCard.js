073d420f384804e6d3cbf803e258f606ad8f7946
n00bgineer <n00bgineer@protonmail.com>
2023-07-02 18:17:00 +0530

    Simplifying feature card component

--- web/src/components/FeaturesCard/FeaturesCard.js
+++ web/src/components/FeaturesCard/FeaturesCard.js
@@ -3,28 +3,60 @@ import React from 'react'
 import './featuresCard.css'
 
 import { ArrowCircleRight } from '@mui/icons-material'
-import { Box, Card, Chip, Typography } from '@mui/material'
+import { Box, Card as MuiCard, Chip, Typography, styled } from '@mui/material'
 
 import { Link } from '@redwoodjs/router'
 
 import IconButton from '../IconButton/IconButton'
 
+// CUSTOM COMPONENTS
+// CUSTOM CARD COMPONENT
+const CustomCard = styled(MuiCard)(({ theme }) => ({
+  '&.feature-card': {
+    aspectRatio: '1.1/1',
+    border: '1px solid',
+    borderRadius: '25px',
+    flexGrow: '1',
+    margin: '15px',
+    maxWidth: 'calc(33% - 50px)',
+    minWidth: '400px',
+    padding: '25px',
+    position: 'relative',
+    borderColor:
+      theme.palette.mode === 'light'
+        ? 'rgb(56, 68, 77)'
+        : 'rgba(230,230,230,0.4)',
+    backdropFilter: 'blur(10px)',
+  },
+  '& .feature-card-title': {
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  '& .feature-card-subtitle': {
+    marginTop: '10px',
+    textAlign: 'left',
+  },
+  '&> .feature-card-image': {
+    bottom: '0',
+    marginBottom: '-20%',
+    marginRight: '-15%',
+    position: 'absolute',
+    right: '0',
+    width: '60%',
+  },
+  '& .feature-tags-container': {
+    marginTop: '15px',
+  },
+  '& .feature-card-link': {
+    marginLeft: '-5px',
+    marginTop: '5px',
+  },
+}))
+
 const FeaturesCard = ({ title, content, image, tags, link }) => {
   return (
-    <Card
-      className="features-card"
-      sx={(theme) => {
-        return {
-          border: '1px solid',
-          borderColor:
-            theme.palette.mode === 'light'
-              ? 'rgb(56, 68, 77)'
-              : 'rgba(230,230,230,0.4)',
-          backdropFilter: 'blur(10px)',
-        }
-      }}
-    >
-      <Box className="features-card-content">
+    <CustomCard className="feature-card">
+      <Box className="feature-card-content">
         <Typography variant="h5" className="feature-card-title">
           {title}
         </Typography>
@@ -33,8 +65,8 @@ const FeaturesCard = ({ title, content, image, tags, link }) => {
         </Typography>
         {tags && (
           <Box className="feature-tags-container">
-            {tags.map((tag) => {
-              return <Chip key={tag} color="default" label={tag} />
+            {tags.map((tag, index) => {
+              return <Chip key={index} color="default" label={tag} />
             })}
           </Box>
         )}
@@ -46,7 +78,7 @@ const FeaturesCard = ({ title, content, image, tags, link }) => {
         )}
       </Box>
       {image && <img src={image} className="feature-card-image" alt={title} />}
-    </Card>
+    </CustomCard>
   )
 }
 
